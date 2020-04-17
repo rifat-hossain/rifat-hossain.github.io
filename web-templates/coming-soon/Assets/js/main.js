46a96dcd8228adac9759909f0eb4e54a6a46fcc8
rifathossain <rifathossainayon@gmail.com>
2020-04-17 18:23:31 +0600

    modified

--- web-templates/coming-soon/Assets/js/main.js
+++ web-templates/coming-soon/Assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
             $('#clock').countdown('2020/10/10', function (event) {
                 var $this = $(this).html(event.strftime(
-                    '<Table\>\
+                    '<Table style=\"width: 100%\;"\>\
                     <tr\>\
                         <td><h1>%w</h1></td\>\
                         <td><h1>%d</h1></td\>\
