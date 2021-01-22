4702a5aeec693f57ec0e4eb6c68aa495d3225ace
rifat-hossain <rifathossainayon@gmail.com>
2021-01-22 16:36:22 +0600

    completed android specialized profile

--- db/lists.js
+++ db/lists.js
@@ -2,16 +2,10 @@ var xhttp = new XMLHttpRequest();
 xhttp.open("GET", "https://rifat-hossain.github.io/db/db.json", true);
 xhttp.send(null);
 
-var pr_count = 1;
-var projss;
+var vjson;
+var n_count = 1;
 var np;
-
-var ex_count = 1;
-var expss;
 var nx;
-
-var ss_count = 1;
-var ssss;
 var ns;
 
 var num = 5;
@@ -19,16 +13,24 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var json = xhttp.response;
         if (document.getElementById("projs") != null) {
-            projss = JSON.parse(json).proj;
+            vjson = JSON.parse(json).proj;
             less_proj();
         }
         if (document.getElementById("exps") != null) {
-            expss = JSON.parse(json).exp;
+            vjson = JSON.parse(json).exp;
             less_exp();
         }
         if (document.getElementById("sss") != null) {
-            ssss = JSON.parse(json).ss;
+            vjson = JSON.parse(json).ss;
             less_ss();
+        }
+        if (document.getElementById("ides") != null) {
+            vjson = JSON.parse(json).idetool;
+            idetool(document.getElementById("ides").getAttribute("value"));
+        }
+        if (document.getElementById("a_proj") != null) {
+            vjson = JSON.parse(json).proj;
+            aproj(document.getElementById("a_proj").getAttribute("value"));
         }
         if (document.getElementById("full") != null) {
             var url = new URL(window.location.href);
@@ -91,12 +93,44 @@ function pageCount(length, num) {
     return i;
 }
 
+function idetool(tag) {
+    document.getElementById("ides").innerHTML = "";
+    for (var i = 0; i < Object.keys(vjson).length; i++) {
+        var cont = false;
+        vjson[i].tag.forEach(element => {
+            if(element == tag){
+                cont = true;
+            }
+        });
+        if(cont == false){
+            continue;
+        }
+        document.getElementById("ides").innerHTML += "<li><strong>" + vjson[i].title + "</strong>&nbsp;- " + vjson[i].des + "</li>";
+    }
+}
+
+function aproj(tag) {
+    document.getElementById("a_proj").innerHTML = "";
+    for (var i = 0; i < Object.keys(vjson).length; i++) {
+        var cont = false;
+        vjson[i].tags.forEach(element => {
+            if(element == tag){
+                cont = true;
+            }
+        });
+        if(cont == false){
+            continue;
+        }
+        document.getElementById("a_proj").innerHTML += "<li><strong>" + vjson[i].name + "</strong>&nbsp;- " + vjson[i].des + "</li>";
+    }
+}
+
 function less_proj() {
 
-    np = pageCount(Object.keys(projss).length, num);
+    np = pageCount(Object.keys(vjson).length, num);
     document.getElementById("proj_count").innerHTML = "<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"prev_pr();\">Previous</a></li>";
     for (var i = 1; i <= np; i++) {
-        if (i == pr_count) {
+        if (i == n_count) {
             document.getElementById("proj_count").innerHTML += "<li class=\"page-item active\"><a class=\"page-link text-light\">" + i + "</a></li>";
         } else {
             document.getElementById("proj_count").innerHTML += "<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"jmp_pr(" + i + ");\">" + i + "</a></li>";
@@ -104,22 +138,22 @@ function less_proj() {
     }
     document.getElementById("proj_count").innerHTML += "<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"nxt_pr();\">Next</a></li>\
 		<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"gotoList('proj');\">View All</a></li>";
-    if (Object.keys(projss).length <= num) {
+    if (Object.keys(vjson).length <= num) {
         document.getElementById("proj_count").setAttribute("class", "invisible pagination justify-content-center");
     }
 
     document.getElementById("projs").innerHTML = "";
-    for (var i = (pr_count - 1) * num; i < Object.keys(projss).length && i < pr_count * num; i++) {
-        document.getElementById("projs").innerHTML += "<li><strong>" + projss[i].name + "</strong>&nbsp;- " + projss[i].des + "</li>";
+    for (var i = (n_count - 1) * num; i < Object.keys(vjson).length && i < n_count * num; i++) {
+        document.getElementById("projs").innerHTML += "<li><strong>" + vjson[i].name + "</strong>&nbsp;- " + vjson[i].des + "</li>";
     }
 }
 
 function less_exp() {
 
-    nx = pageCount(Object.keys(expss).length, num);
+    nx = pageCount(Object.keys(vjson).length, num);
     document.getElementById("exp_count").innerHTML = "<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"prev_ex();\">Previous</a></li>";
     for (var i = 1; i <= nx; i++) {
-        if (i == ex_count) {
+        if (i == n_count) {
             document.getElementById("exp_count").innerHTML += "<li class=\"page-item active\"><a class=\"page-link text-light\">" + i + "</a></li>";
         } else {
             document.getElementById("exp_count").innerHTML += "<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"jmp_ex(" + i + ");\">" + i + "</a></li>";
@@ -127,93 +161,93 @@ function less_exp() {
     }
     document.getElementById("exp_count").innerHTML += "<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"nxt_ex();\">Next</a></li>\
 		<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"gotoList('exp');\">View All</a></li>";
-    if (Object.keys(expss).length <= num) {
+    if (Object.keys(vjson).length <= num) {
         document.getElementById("exp_count").setAttribute("class", "invisible pagination justify-content-center");
     }
 
     document.getElementById("exps").innerHTML = "";
-    for (var i = (ex_count - 1) * num; i < Object.keys(expss).length && i < ex_count * num; i++) {
-        document.getElementById("exps").innerHTML += "<li><strong>" + expss[i].name + "</strong>&nbsp;- " + expss[i].des + "</li>";
+    for (var i = (n_count - 1) * num; i < Object.keys(vjson).length && i < n_count * num; i++) {
+        document.getElementById("exps").innerHTML += "<li><strong>" + vjson[i].name + "</strong>&nbsp;- " + vjson[i].des + "</li>";
     }
 }
 
 function less_ss() {
 
-    ns = pageCount(Object.keys(ssss).length, num);
-    document.getElementById("ss_count").innerHTML = "<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"prev_ss();\">Previous</a></li>";
+    ns = pageCount(Object.keys(vjson).length, num);
+    document.getElementById("n_count").innerHTML = "<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"prev_ss();\">Previous</a></li>";
     for (var i = 1; i <= ns; i++) {
-        if (i == ex_count) {
-            document.getElementById("ss_count").innerHTML += "<li class=\"page-item active\"><a class=\"page-link text-light\">" + i + "</a></li>";
+        if (i == n_count) {
+            document.getElementById("n_count").innerHTML += "<li class=\"page-item active\"><a class=\"page-link text-light\">" + i + "</a></li>";
         } else {
-            document.getElementById("ss_count").innerHTML += "<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"jmp_ss(" + i + ");\">" + i + "</a></li>";
+            document.getElementById("n_count").innerHTML += "<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"jmp_ss(" + i + ");\">" + i + "</a></li>";
         }
     }
-    document.getElementById("ss_count").innerHTML += "<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"nxt_ss();\">Next</a></li>\
+    document.getElementById("n_count").innerHTML += "<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"nxt_ss();\">Next</a></li>\
 		<li class=\"page-item\"><a class=\"page-link text-primary\" onclick=\"gotoList('ss');\">View All</a></li>";
-    if (Object.keys(ssss).length <= num) {
-        document.getElementById("ss_count").setAttribute("class", "invisible pagination justify-content-center");
+    if (Object.keys(vjson).length <= num) {
+        document.getElementById("n_count").setAttribute("class", "invisible pagination justify-content-center");
     }
 
     document.getElementById("sss").innerHTML = "";
-    for (var i = (ex_count - 1) * num; i < Object.keys(ssss).length && i < ex_count * num; i++) {
-        document.getElementById("sss").innerHTML += "<li><strong>" + ssss[i].name + "</strong>&nbsp;- " + ssss[i].des + "</li>";
+    for (var i = (n_count - 1) * num; i < Object.keys(vjson).length && i < n_count * num; i++) {
+        document.getElementById("sss").innerHTML += "<li><strong>" + vjson[i].name + "</strong>&nbsp;- " + vjson[i].des + "</li>";
     }
 }
 
 function prev_pr() {
-    if (pr_count > 1) {
-        pr_count--;
+    if (n_count > 1) {
+        n_count--;
         less_proj();
     }
 }
 
 function nxt_pr() {
-    if (pr_count < np) {
-        pr_count++;
+    if (n_count < np) {
+        n_count++;
         less_proj();
     }
 }
 
 function jmp_pr(i) {
-    pr_count = i;
+    n_count = i;
     less_proj();
 }
 
 function prev_ex() {
-    if (ex_count > 1) {
-        ex_count--;
+    if (n_count > 1) {
+        n_count--;
         less_exp();
     }
 }
 
 function nxt_ex() {
-    if (ex_count < nx) {
-        ex_count++;
+    if (n_count < nx) {
+        n_count++;
         less_exp();
     }
 }
 
 function jmp_ex(i) {
-    ex_count = i;
+    n_count = i;
     less_exp();
 }
 
 function prev_ss() {
-    if (ss_count > 1) {
-        ss_count--;
+    if (n_count > 1) {
+        n_count--;
         less_ss();
     }
 }
 
 function nxt_ss() {
-    if (ss_count < np) {
-        ss_count++;
+    if (n_count < np) {
+        n_count++;
         less_ss();
     }
 }
 
 function jmp_ss(i) {
-    ss_count = i;
+    n_count = i;
     less_ss();
 }
 
